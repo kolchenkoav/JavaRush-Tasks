66d27a95d4384a1627d612bd1de7edc57aafadad
Aleksey <kolchenkoav@gmail.com>
2022-06-30 18:50:42 +0300

    JavaRush Tasks 30.06.2022 v012

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-
         List<String> list = new CustomTree();
 
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        System.out.println("The list size is " + list.size());
+        System.out.println("The expected parent is 8. The actual parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
+
+        System.out.println("The expected parent is 10. The actual parent is " + ((CustomTree) list).getParent("10"));
     }
 }
